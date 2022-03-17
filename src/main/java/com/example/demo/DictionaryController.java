e3d7d268f437ab06ab8bd1418e994a9c2fb8c55e
Faraz Durrani <farazkhurram@msn.com>
2022-03-16 19:41:43 -0500

    externalized key

--- src/main/java/com/example/demo/DictionaryController.java
+++ src/main/java/com/example/demo/DictionaryController.java
@@ -1,30 +1,33 @@
 package com.example.demo;
 
 import com.github.wnameless.json.flattener.JsonFlattener;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
 @Controller
-public class MyController {
+public class DictionaryController {
 
-  private static final String KEY = "c3d51672-3f28-4615-83d7-475859de6989";
-  private static final String URL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/%s?key=%s";
+  private final RestTemplate restTemplate;
+  private final String key;
+  private final String url;
 
-  @Autowired
-  private RestTemplate restTemplate;
+  public DictionaryController(RestTemplate restTemplate, @Value("${key}") String key,
+                              @Value("${url}") String url) {
+    this.restTemplate = restTemplate;
+    this.key = key;
+    this.url = url;
+  }
 
   @GetMapping("/{word}")
   public String get(@PathVariable String word, Model model) {
-    ResponseEntity<String> response = restTemplate.getForEntity(String.format(URL, word, KEY), String.class);
+    ResponseEntity<String> response = restTemplate.getForEntity(String.format(url, word, key), String.class);
     String json = response.getBody();
     Map<String, Object> flattenJson = JsonFlattener.flattenAsMap(json);
     flattenJson.keySet().removeIf(x -> !x.contains("shortdef"));
