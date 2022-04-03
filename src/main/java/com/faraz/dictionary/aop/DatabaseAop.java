cc643045c0db6a3b03bc1f9bf4b3d5fac7e37dee
Faraz Durrani <farazkhurram@msn.com>
2022-04-03 08:34:36 -0500

    data restored. aop is functional

--- src/main/java/com/faraz/dictionary/aop/DatabaseAop.java
+++ src/main/java/com/faraz/dictionary/aop/DatabaseAop.java
@@ -4,6 +4,9 @@ import com.faraz.dictionary.entity.Dictionary;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
@@ -13,35 +16,64 @@ import static java.util.Objects.nonNull;
 @Aspect
 @Component
 public class DatabaseAop {
+
+  private static final Logger logger = LoggerFactory.getLogger(DatabaseAop.class);
+
   @Around("bean(dictionaryRepository) && execution(* saveAll(..))")
   public Object interceptSaveAll(ProceedingJoinPoint pjp) throws Throwable {
+    logger.info("Start of Servicing " + ((MethodSignature) pjp.getSignature()).getMethod());
     massage(pjp.getArgs());
     Object retVal = pjp.proceed();
     massage(new Object[]{retVal});
+    logger.info("End of Servicing " + ((MethodSignature) pjp.getSignature()).getMethod());
     return retVal;
   }
 
   @Around("bean(dictionaryRepository) && execution(* findAll(..))")
   public Object interceptFindAll(ProceedingJoinPoint pjp) throws Throwable {
+    logger.info("Start of Servicing " + ((MethodSignature) pjp.getSignature()).getMethod());
     Object retVal = pjp.proceed();
     massage(new Object[]{retVal});
+    logger.info("End of Servicing " + ((MethodSignature) pjp.getSignature()).getMethod());
+    return retVal;
+  }
+
+  @Around("bean(mongoTemplate) && (execution(* save(..)) || execution(* insert(..)))")
+  public Object interceptInsertions(ProceedingJoinPoint pjp) throws Throwable {
+    logger.info("Start of Servicing " + ((MethodSignature) pjp.getSignature()).getMethod());
+    Object[] objects = pjp.getArgs();
+    massage(objects);
+    Object retVal = pjp.proceed();
+    massage(new Object[]{retVal});
+    logger.info("End of Servicing " + ((MethodSignature) pjp.getSignature()).getMethod());
+    return retVal;
+  }
+
+  @Around("bean(mongoTemplate) && execution(* find(..)))")
+  public Object interceptInsertions2(ProceedingJoinPoint pjp) throws Throwable {
+    logger.info("Start of Servicing " + ((MethodSignature) pjp.getSignature()).getMethod());
+    Object retVal = pjp.proceed();
+    massage(new Object[]{retVal});
+    logger.info("End of Servicing " + ((MethodSignature) pjp.getSignature()).getMethod());
     return retVal;
   }
 
   private void massage(Object[] args) {
-    Collection col = (Collection) args[0];
-    for (Object o : col) {
-      Dictionary dictionary = (Dictionary) o;
-      if (nonNull(dictionary.getWord())) {
-        dictionary.setWord(dictionary.getWord().trim().toLowerCase());
+    if (args[0] instanceof Collection) {
+      Collection col = (Collection) args[0];
+      for (Object o : col) {
+        massageDictionary(o);
       }
+    }
+    if (args[0] instanceof Dictionary) {
+      massageDictionary(args[0]);
     }
   }
 
-  @Around("bean(mongoTemplate) && (execution(* save(..)) || execution(* insert(..)))")
-  public Object intercepInsertions(ProceedingJoinPoint pjp) throws Throwable {
-    Object[] objects = pjp.getArgs();
-    Object retVal = pjp.proceed();
-    return retVal;
+  private void massageDictionary(Object o) {
+    Dictionary dictionary = (Dictionary) o;
+    if (nonNull(dictionary.getWord())) {
+      dictionary.setWord(dictionary.getWord().trim().toLowerCase());
+    }
   }
 }
