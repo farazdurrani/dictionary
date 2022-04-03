1c74766d469ead3d39653d05e999f5cbde444584
Faraz Durrani <farazkhurram@msn.com>
2022-04-02 23:35:20 -0500

    messed up big time

--- src/main/java/com/faraz/dictionary/batch/SetLowercaseAndLoadReminder.java
+++ src/main/java/com/faraz/dictionary/batch/SetLowercaseAndLoadReminder.java
@@ -13,9 +13,13 @@ import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static java.util.Objects.nonNull;
 
 @Component
-@Profile("reminder")
+//@Profile("reminder")
 public class SetLowercaseAndLoadReminder {
 
   private static final Logger logger = LoggerFactory.getLogger(SetLowercaseAndLoadReminder.class);
@@ -25,7 +29,8 @@ public class SetLowercaseAndLoadReminder {
 
   @PostConstruct
   public void saveUniqueAndSetReminders() throws IOException {
-    List<Dictionary> words = dictionaryRepository.findAll();
+    List<Dictionary> words = dictionaryRepository.findAll().stream().filter(Objects::nonNull).filter(
+        d -> nonNull(d.getWord())).collect(Collectors.toList());
     logger.info("Words loaded: {}", words.size());
     Map<String, Dictionary> map = new LinkedHashMap<>();
     for (Dictionary word : words) {
@@ -36,6 +41,8 @@ public class SetLowercaseAndLoadReminder {
       _word.setReminded(false);
       map.put(word.getWord().trim().toLowerCase(), _word);
     }
+    logger.info("Words reloaded: {}", map.values().size());
+//    dictionaryRepository.deleteAll();
     List<Dictionary> saved = dictionaryRepository.saveAll(map.values());
     logger.info("Words transformed: {}", map.values().size());
     logger.info("Finished loading reminders all at once");
